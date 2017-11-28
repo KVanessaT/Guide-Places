b57f33764e86f608773633c3d99831bec781bd6b
KVanessaT <31851724+KVanessaT@users.noreply.github.com>
2017-11-28 07:37:32 -0500

    Cambios en el script

--- modificacion modelo/nscript (1).sql
+++ modificacion modelo/nscript (1).sql
@@ -1,65 +1,65 @@
-create database GUIDE_PLACES DEFAULT CHARACTER SET UTF8 COLLATE UTF8_UNICODE_CI;
-USE GUIDE_PLACES;
+create database GUIDEPLACES DEFAULT CHARACTER SET UTF8 COLLATE UTF8_UNICODE_CI;
+USE GUIDEPLACES;
 
-CREATE TABLE USUARIOS(
-id_usuario int not null auto_increment,
-nombre_usuario varchar (30) not null,
-apellido_usuario varchar (30) not null,
-edad_usuario int (2) not null,
-email_usuario varchar(60) not null,
-PRIMARY KEY (id_usuario)
+CREATE TABLE Usuarios(
+idUsuario int not null auto_increment,
+nombreUsuario varchar (30) not null,
+apellidoUsuario varchar (30) not null,
+edadUsuario int (2) not null,
+emailUsuario varchar(60) not null,
+PRIMARY KEY (idUsuario)
 );
 
-CREATE TABLE REGISTRO(
-id_registro int not null auto_increment,
-id_usuario int,
+CREATE TABLE Registro(
+idRegistro int not null auto_increment,
+idUsuario int,
 clave BLOB NULL,
-PRIMARY KEY (id_registro)
+PRIMARY KEY (idRegistro)
 );
 
-CREATE TABLE LUGARES(
-id_lugar int not null auto_increment,
-nombre_lugar varchar (30) not null,
-PRIMARY KEY (id_lugar)
+CREATE TABLE Lugares(
+idLugar int not null auto_increment,
+nombreLugar varchar (30) not null,
+PRIMARY KEY (idLugar)
 );
 
-CREATE TABLE PROVINCIAS(
-id_provincia int not null auto_increment,
-nombre_provincia varchar (30) not null,
-PRIMARY KEY (id_provincia )
+CREATE TABLE Provincia(
+idProvincia int not null auto_increment,
+nombreProvincia varchar (30) not null,
+PRIMARY KEY (idProvincia )
 );
 
-CREATE TABLE DETALLES(
-id_detalle int not null auto_increment,
+CREATE TABLE Detalles(
+idDetalle int not null auto_increment,
 descripcion varchar (900) not null,
-PRIMARY KEY (id_detalle )
+PRIMARY KEY (idDetalle )
 );
 
-CREATE TABLE INFORMACION_COMPLETA(
-id_infcom int not null auto_increment,
-id_lugar int not null,
-id_provincia int not null,
-id_detalle  int,
-datosgoogle varchar (1000) not null,
-PRIMARY KEY (id_infcom )
+CREATE TABLE InformacionCompleta(
+idInfcom int not null auto_increment,
+idLugar int not null,
+idProvincia int not null,
+idDetalle  int,
+datosGoogle varchar (1000) not null,
+PRIMARY KEY (idInfcom )
 );
 
 ALTER TABLE REGISTRO
 ADD CONSTRAINT R_1 
-FOREIGN KEY (id_usuario) 
-REFERENCES USUARIOS (id_usuario);
+FOREIGN KEY (idUsuario) 
+REFERENCES USUARIOS (idUsuario);
 
-ALTER TABLE INFORMACION_COMPLETA
+ALTER TABLE InformacionCompleta
 ADD CONSTRAINT R_2 
-FOREIGN KEY (id_lugar) 
-REFERENCES LUGARES (id_lugar);
+FOREIGN KEY (idLugar) 
+REFERENCES LUGARES (idLugar);
 
-ALTER TABLE INFORMACION_COMPLETA
+ALTER TABLE InformacionCompleta
 ADD CONSTRAINT R_3
-FOREIGN KEY (id_provincia) 
-REFERENCES PROVINCIAS (id_provincia);
+FOREIGN KEY (idProvincia) 
+REFERENCES PROVINCIAS (idProvincia);
 
-ALTER TABLE INFORMACION_COMPLETA
+ALTER TABLE InformacionCompleta
 ADD CONSTRAINT R_4
-FOREIGN KEY (id_detalle) 
-REFERENCES DETALLES (id_detalle);
+FOREIGN KEY (idDetalle) 
+REFERENCES DETALLES (idDetalle);
