7900422a9da4e707d6bcea99af3af089d4f9ba51
KVanessaT <31851724+KVanessaT@users.noreply.github.com>
2017-12-04 23:10:22 -0500

    host

--- server/config/ConexionBaseDatos.php
+++ server/config/ConexionBaseDatos.php
@@ -6,6 +6,8 @@ class ConexionBaseDatos {
         $array = array();
         $array[] = new DatosConexion("pruebasITSY","172.16.0.9","ignug","prueba","12345678");
         $array[] = new DatosConexion("local","localhost","ignug","prueba","12345678");
+        $array[] = new DatosConexion("local","sql201.byethost7.com","b7_21146743_GuidePlaces","b7_21146743","ILOVEKOREA25");
+        
         return $array;
     }
 }
